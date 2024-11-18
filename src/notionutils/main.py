2d50b9047b54bd9ee387769bc2fe2bec3c573b6a
Suhail <suhailece@gmail.com>
2024-11-17 16:51:25 -0800

    update main.py

--- src/notionutils/main.py
+++ src/notionutils/main.py
@@ -1,26 +1,29 @@
+import re
 from oauthmanager import AuthManager, OnePasswordAuthManager
 from notion import NotionManager
 
 def main():
+    # Prompt the user for the Notion database URL and image URL
+    database_url = input("Please enter the Notion database URL: ")
+    image_url = input("Please enter the cover image URL: ")
+
+    # Use regex to extract the DATABASE_ID from the URL
+    match = re.search(r"\/([a-f0-9]{32})\?", database_url)
+    if not match:
+        print("Invalid Notion database URL provided. Please try again.")
+        return
+    DATABASE_ID = match.group(1)
+
     # Initialize the AuthManager
     auth_manager = OnePasswordAuthManager()
 
     # Retrieve credentials for Notion
     notion_creds = auth_manager.get_credentials("Notion", "credential")
-
     NOTION_API_KEY = notion_creds['credential']
-    DATABASE_ID = "13ca1865b187803390f4d1b6b0bd92e8"
-
-    # Cover image URL (broken down for better readability)
-    new_cover_url = (
-        "https://images.unsplash.com/photo-1690321607902-2799a1e8eaaa"
-        "?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid="
-        "M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    )
 
     # Create an instance of NotionManager and update the cover image
     install_db = NotionManager(NOTION_API_KEY, DATABASE_ID)
-    install_db.update_cover_image(cover_url=new_cover_url)
+    install_db.update_cover_image(cover_url=image_url)
 
 if __name__ == "__main__":
     main()
