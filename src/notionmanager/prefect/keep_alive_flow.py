84f3b40c286bd515e3ada9f3f7b398bb15f41270
Suhail <suhailece@gmail.com>
2025-02-08 20:16:16 -0800

    udpate scripts

--- src/notionmanager/prefect/keep_alive_flow.py
+++ src/notionmanager/prefect/keep_alive_flow.py
@@ -1,5 +1,5 @@
 from prefect import flow, task, get_run_logger
-from src.notionutils.supabase.client import SupabaseClient
+from src.notionmanager.supabase.client import SupabaseClient
 
 @task
 def keep_supabase_active(env_path: str = "/opt/prefect/.env"):
